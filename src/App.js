0735331634dbe95f6c2ad5e234c8b9e9be1679d1
mihir123456789 <109252112+mihir123456789@users.noreply.github.com>
2022-11-03 02:18:22 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch(' https://immense-reaches-52663.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -73,7 +73,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch(' https://immense-reaches-52663.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
